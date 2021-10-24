42baeb928ca7c9ad8435500b4be7db3f93b240bb
Rasmus Buchholdt <rasmus.buchholdt@gmail.com>
2021-10-24 19:14:53 +0200

    Only announce host is down once and announce once it is up again

--- src/app.ts
+++ src/app.ts
@@ -8,12 +8,19 @@ const CRON_SCHEDULE: string = config.cron_schedule;
 const pingCfg = config.ping_cfg;
 
 class Watchdog {
+
+  private downedHosts: string[] = [];
+
   start() {
     cron.schedule(CRON_SCHEDULE, () => {
       config.hosts.forEach((host: string) => {
         ping.sys.probe(host, (isAlive: boolean) => {
-          if (!isAlive) {
-            sendWebhookMessage('Server Status', `${host} is down!`)
+          if (isAlive && this.downedHosts.includes(host)) {
+            sendWebhookMessage('Server Status', `${host} is up again!`);
+            this.downedHosts.splice(this.downedHosts.indexOf(host), 1);
+          } else if (!isAlive && !this.downedHosts.includes(host)) {
+            this.downedHosts.push(host);
+            sendWebhookMessage('Server Status', `${host} is down!`);
           }
         }, pingCfg);
       });
