91c18068d39ca570952c7f603ae550dff1cde72d
Rasmus Buchholdt <rasmus.buchholdt@gmail.com>
2021-10-24 16:31:14 +0200

    Add support for pinging array of hosts

--- src/_utils/discord-webhook.ts
+++ src/_utils/discord-webhook.ts
@@ -2,13 +2,12 @@ const { Webhook, MessageBuilder } = require('discord-webhook-node');
 const config = require('../../config/app.json');
 const hook = new Webhook(config.hook_url);
 
-export function sendWebhookMessage(title: string, message: string, footer: string): void {
+export function sendWebhookMessage(title: string, message: string): void {
     const embed = new MessageBuilder()
         .setTitle(title)
         .setAuthor('Bot boy', 'https://cdn.discordapp.com/embed/avatars/0.png', 'https://www.google.com')
         .setColor('#00b0f4')
         .setDescription(message)
-        .setFooter(footer)
         .setTimestamp();
     hook.send(embed);
 }
--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,7 @@
+import { sendWebhookMessage } from './_utils/discord-webhook';
+
 const cron = require('node-cron');
+const ping = require('ping');
 const config = require('../config/app.json');
 
 const CRON_SCHEDULE: string = config.cron_schedule;
@@ -6,7 +9,13 @@ const CRON_SCHEDULE: string = config.cron_schedule;
 class Watchdog {
   start() {
     cron.schedule(CRON_SCHEDULE, () => {
-      console.log('Scheduled');
+      config.hosts.forEach((host: string) => {
+        ping.sys.probe(host, (isAlive: boolean) => {
+          if (!isAlive) {
+            sendWebhookMessage('Server Status', `${host} is down!`)
+          }
+        });
+      });
     });
   }
 }
